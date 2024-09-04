bcdd708fca0e053cae8159affa37ba3f627316a0
rhcehd <lhs9403@naver.com>
2024-09-04 18:47:30 +0900

    Add receipt download function

--- npay-receipt-issuance.py
+++ npay-receipt-issuance.py
@@ -1,32 +1,60 @@
-import pyautogui
+import os
+
 from selenium import webdriver
+from selenium.common import NoSuchElementException, StaleElementReferenceException
+from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-import os
+from datetime import datetime
+import pyautogui
 import time
 
 driver: webdriver.Chrome
 base_url = "https://order.pay.naver.com/home"
-current_url = ""
-date_start = ""
-date_end = ""
-
+download_path = os.path.join(os.path.join(os.getcwd(), "pdf"))
 
 def initialize_driver():
     global driver
     # Chrome 옵션 설정
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--start-maximized")
+    settings = {
+        "recentDestinations": [
+            {
+                "id": "Save as PDF",
+                "origin": "local",
+                "account": "",
+            }
+        ],
+        "selectedDestinationId": "Save as PDF",
+        "version": 2,
+    }
+    if not os.path.exists(download_path):
+        os.mkdir(download_path)
+    prefs = {
+        'printing.print_preview_sticky_settings.appState': settings,
+        'savefile.default_directory': download_path  # PDF 파일이 저장될 경로
+    }
 
-    service = ChromeService(ChromeDriverManager().install())
+    chrome_options.add_experimental_option('prefs', prefs)
+    chrome_options.add_argument('--kiosk-printing')  # 인쇄 대화상자 자동 확인
+
+    driver_path = ChromeDriverManager().install()
+    service = ChromeService(os.path.join(os.path.dirname(driver_path), "chromedriver.exe"))
     driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.implicitly_wait(3)
 
 
 def start_driver(url: str):
     driver.get(url)
     global current_url
     current_url = driver.current_url
+
+
+def move_to_last_window():
+    window_handles = driver.window_handles
+    driver.switch_to.window(window_handles[len(window_handles) - 1])
 
 
 def wait_for_login():
@@ -36,17 +64,161 @@ def wait_for_login():
         time.sleep(1)
 
 
+def set_filter():
+    try:
+        search_btn = driver.find_element(By.CSS_SELECTOR, "#root > div > div.TwoStagedLayoutWrapper-module_container__-NuVi > div > div.TwoStagedLayoutWrapper-module_content-left__2eGzo.PcHome_content-left__1Sj2x > div.PaymentCategoryAndSearchButton_article__3lLC7 > div > div.PaymentCategoryAndSearchButton_area-button__3G-wX > button")
+        search_btn.click()
+        is_search_modal_showing = True
+        while is_search_modal_showing:
+            try:
+                driver.find_element(By.XPATH, "//*[contains(@id, 'portal__')]/div/div[1]/div[2]/div[5]/button")
+            except NoSuchElementException:
+                search_tags = driver.find_elements(By.XPATH, "//*[@id='root']/div/div[2]/div/div[1]/div[1]/div/div[2]/div/ul")
+                if len(search_tags) != 0:
+                    is_search_modal_showing = False
+                time.sleep(1)
+        return True
+    except NoSuchElementException as e:
+        return False
+
+
+def card_receipt():
+    purchase_receipt_btn = driver.find_element(By.XPATH, "//*[@id='content']/div[1]/div/div[2]")
+    purchase_receipt_btn.click()
+    time.sleep(1)
+
+    datetime_raw = driver.find_element(By.XPATH, "//*[@id='container']/div/ul/li[2]").text
+    date = datetime.strptime(datetime_raw.split('\n')[1], "%Y.%m.%d %H:%M:%S").strftime("%m%d")
+    price = driver.find_element(By.XPATH, "//*[@id='container']/div/ul/li[4]/span").text.replace("원", "")
+    receipt_count = 1
+
+    receipt_path = os.path.join(download_path, f"{date}_{price}")
+    if not os.path.exists(receipt_path):
+        os.mkdir(receipt_path)
+
+    print_btn = driver.find_element(By.XPATH, "//*[@id='container']/div/div/button")
+    print_btn.click()
+    time.sleep(1)
+    pyautogui.write(message=f"{receipt_path}\\{date}_{price}_{receipt_count}")
+    pyautogui.press('enter')
+    pyautogui.press('y')
+    receipt_count += 1
+
+    time.sleep(1)
+    back_btn = driver.find_element(By.XPATH, "//*[@id='header']/div/a")
+    back_btn.click()
+    time.sleep(1)
+    card_receipt_btns = driver.find_elements(By.CLASS_NAME, "button_area")
+    card_receipt_btns = card_receipt_btns[1:]
+    for card_receipt_btn in card_receipt_btns:
+        card_receipt_btn.find_element(By.XPATH, "a").click()
+        time.sleep(1)
+        print_btn = driver.find_element(By.XPATH, "//*[@id='receipt']/div/div[2]/div[2]/button")
+        print_btn.click()
+        time.sleep(1)
+        pyautogui.write(message=f"{receipt_path}\\{date}_{price}_{receipt_count}")
+        pyautogui.press('enter')
+        pyautogui.press('y')
+        receipt_count += 1
+        time.sleep(1)
+        back_btn = driver.find_element(By.XPATH, "//*[@id='receipt']/header/div/div/button")
+        back_btn.click()
+        time.sleep(1)
+
+
+def cash_receipt():
+    datetime_raw = driver.find_element(By.XPATH, "//*[@id='receipt']/div[2]/div[1]/div[1]/dl/div[5]/dd").text
+    date = datetime.strptime(datetime_raw, "%Y.%m.%d %H:%M:%S").strftime("%m%d")
+    price = driver.find_element(By.XPATH, "//*[@id='receipt']/div[2]/div[1]/div[4]/div").text
+
+    receipt_path = os.path.join(download_path, f"{date}_{price}")
+    if not os.path.exists(receipt_path):
+        os.mkdir(receipt_path)
+
+    purchase_receipt_btn = driver.find_element(By.XPATH, "//*[@id='receipt']/div[2]/div[2]/div[2]")
+    purchase_receipt_btn.click()
+    time.sleep(1)
+    pyautogui.write(message=f"{receipt_path}\\{date}_{price}")
+    pyautogui.press('enter')
+    pyautogui.press('y')
+    time.sleep(1)
+
+
+def detail():
+    receipt_btn = driver.find_element(By.XPATH, "//*[@id='content']/div/div[2]/div/div/button")
+    receipt_btn.click()
+    time.sleep(1)
+    move_to_last_window()
+
+    try:
+        card_receipt()
+    except NoSuchElementException:
+        print('no card receipt')
+        try:
+            cash_receipt()
+        except NoSuchElementException:
+            print('no cash receipt')
+    driver.close()
+    move_to_last_window()
+
+
 def a():
+    is_filter_set = False
+    while not is_filter_set:
+        time.sleep(1)
+        is_filter_set = set_filter()
     time.sleep(1)
-    search_btn = driver.find_element(By.CSS_SELECTOR, "#root > div > div.TwoStagedLayoutWrapper-module_container__-NuVi > div > div.TwoStagedLayoutWrapper-module_content-left__2eGzo.PcHome_content-left__1Sj2x > div.PaymentCategoryAndSearchButton_article__3lLC7 > div > div.PaymentCategoryAndSearchButton_area-button__3G-wX > button")
-    search_btn.click()
+
+    while True:
+        try:
+            show_more_btn = driver.find_element(By.XPATH, "//*[@id='root']/div/div[2]/div/div[1]/div[3]/div[2]/button")
+            show_more_btn.click()
+            time.sleep(1)
+        except NoSuchElementException:
+            break
+        except StaleElementReferenceException:
+            time.sleep(1)
     time.sleep(1)
-    start_date = driver.find_element(By.XPATH, "//*[contains(@id, 'portal__')]/div/div[1]/div[2]/div[1]/div/div[1]/div[1]")
-    end_date = driver.find_element(By.XPATH, "//*[contains(@id, 'portal__')]/div/div[1]/div[2]/div[1]/div/div[3]/div[1]")
-    driver.execute_script("arguments[0].childNodes[1].nodeValue = '24. 06. 24';", start_date)
-    driver.execute_script("arguments[0].childNodes[1].nodeValue = '24. 07. 01';", end_date)
-    # start_date.send_keys("24. 06. 25")
-    # end_date.send_keys("2024-07-01")
+
+    items = driver.find_elements(By.XPATH, "//*[@id='root']/div/div[2]/div/div[1]/div[3]/div[1]/ul/li")
+    for item in items:
+        retry_count = 0
+        is_failure = False
+        while retry_count < 5:
+            try:
+                time.sleep(1)
+                item_detail = item.find_element(By.XPATH, "div/div[2]")
+                ActionChains(driver) \
+                    .key_down(Keys.CONTROL) \
+                    .click(item_detail) \
+                    .key_up(Keys.CONTROL) \
+                    .perform()
+                time.sleep(1)
+                move_to_last_window()
+                detail()
+                time.sleep(1)
+                driver.close()
+                move_to_last_window()
+            except NoSuchElementException as e:
+                print(e)
+                class_name = item.get_attribute('class')
+                if 'ListBanner' in class_name:
+                    break
+                else:
+                    is_failure = True
+            except StaleElementReferenceException as e:
+                print(e)
+                is_failure = True
+            finally:
+                if len(driver.window_handles) != 1:
+                    driver.close()
+                    move_to_last_window()
+                if is_failure:
+                    is_failure = False
+                    retry_count += 1
+                    print(f"retry...{retry_count}")
+                else:
+                    break
 
 
 def hey():
